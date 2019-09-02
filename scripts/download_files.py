8389f34f7c52fa2ff69bdf9baf55cd00b3320793
Ryota Yamada <roy29fuku@gmail.com>
2019-09-02 17:15:11 +0900

    modify: ファイル名からタイムスタンプ削除

--- scripts/download_files.py
+++ scripts/download_files.py
@@ -4,25 +4,21 @@ from urllib.parse import urlparse
 from ftplib import FTP
 
 
-def download(url, save_dir):
+def download(url, file_name, dir_name):
     print('download from: {}'.format(url))
     res = requests.get(url)
-    dispositoin = res.headers['content-disposition']
-    file_name = dispositoin.split('filename=')[-1].strip('"')
-    with open(os.path.join(save_dir, file_name), 'w') as f:
+    with open(os.path.join(dir_name, file_name), 'w') as f:
         f.write(res.text)
     return
 
 
-def download_via_ftp(url, save_dir):
+def download_via_ftp(url, file_name, dir_name):
     print('download from: {}'.format(url))
     host = urlparse(url).netloc
     ftp_file_path = urlparse(url).path
-    file_name = os.path.split(ftp_file_path)[-1]
-
     ftp = FTP(host)
     ftp.login()
-    with open(os.path.join(save_dir, file_name), "w") as f:
+    with open(os.path.join(dir_name, file_name), "w") as f:
         ftp.retrlines('RETR ' + ftp_file_path, f.write)
     ftp.quit()
     return
@@ -32,7 +28,6 @@ save_dir = 'data/raw'
 if not os.path.isdir(save_dir):
     os.makedirs(save_dir)
 
-download('https://www.cancerrxgene.org/downloads/download/ic', save_dir)
-download('https://www.cancerrxgene.org/downloads/download/anova', save_dir)
-download_via_ftp('ftp://ftp.expasy.org/databases/cellosaurus/cellosaurus.txt', save_dir)
-
+download('https://www.cancerrxgene.org/downloads/download/ic', 'PANCANCER_IC.csv', save_dir)
+download('https://www.cancerrxgene.org/downloads/download/anova', 'PANCANCER_ANOVA.csv', save_dir)
+download_via_ftp('ftp://ftp.expasy.org/databases/cellosaurus/cellosaurus.txt', 'cellosaurus.txt', save_dir)
